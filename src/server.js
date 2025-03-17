0397a9ba205de3e797de093ef74623dcceebf9fc
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-17 11:14:54 +0200

    add cookie-parser middleware for handling cookies

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import { getEnvVar } from "./utils/getEnvVar.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import { notFoundHandler } from "./middlewares/notFoundHandler.js";
 import router from "./routers/index.js";
+import cookieParser from "cookie-parser";
 
 const PORT = Number(getEnvVar("PORT", "3000"));
 
@@ -13,6 +14,7 @@ export function startServer() {
 
   app.use(express.json());
   app.use(cors());
+  app.use(cookieParser());
 
   app.use(
     pino({
