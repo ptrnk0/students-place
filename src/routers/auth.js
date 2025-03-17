f45b0c1783a16190b91fb14414d6ef80bc2a5cc5
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-17 11:47:26 +0200

    add logout route to authRouter for user session termination

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -4,6 +4,7 @@ import { loginUserSchema, registerUserSchema } from "../validation/auth.js";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 import {
   loginUserController,
+  logoutUserController,
   registerUserController,
 } from "../controllers/auth.js";
 
@@ -20,5 +21,7 @@ authRouter.post(
   validateBody(loginUserSchema),
   ctrlWrapper(loginUserController)
 );
+
+authRouter.post("/logout", ctrlWrapper(logoutUserController));
 
 export default authRouter;
