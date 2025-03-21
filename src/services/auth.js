b27e32dd66dd64ad2607ef50002659c790ada917
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-21 11:31:31 +0200

    add session management functions for creating and refreshing user sessions

--- src/services/auth.js
+++ src/services/auth.js
@@ -53,3 +53,41 @@ export async function logoutUser(sessionId) {
   const session = await UsersCollection.deleteOne({ _id: sessionId });
   return session;
 }
+
+export async function createSession() {
+  const accessToken = randomBytes(30).toString("base64");
+  const refreshToken = randomBytes(30).toString("base64");
+
+  return {
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES),
+    refreshTokenValidUntil: new Date(Date.now() + ONE_DAY),
+  };
+}
+
+export async function refreshUserSession(sessionId, refreshToken) {
+  const session = await SessionsCollection.findOne({
+    _id: sessionId,
+    refreshToken,
+  });
+
+  if (!session) {
+    throw createHttpError(401, "Session not found");
+  }
+
+  const isSessionTokenExpired =
+    new Date() > new Date(session.refreshTokenValidUntil);
+
+  if (isSessionTokenExpired) {
+    throw createHttpError(401, "Session token expired");
+  }
+
+  const newSession = createSession();
+  await SessionsCollection.deleteOne({ _id: sessionId, refreshToken });
+
+  return await SessionsCollection.create({
+    userId: session.userId,
+    ...newSession,
+  });
+}
