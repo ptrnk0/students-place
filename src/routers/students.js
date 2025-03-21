f3c47c5944d7da12ef8e7feee0afdbedb9c6d2e5
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-21 13:25:49 +0200

    add role-based access control to student routes for TEACHER and PARENT roles

--- src/routers/students.js
+++ src/routers/students.js
@@ -14,29 +14,39 @@ import {
 } from "../validation/students.js";
 import { isValidId } from "../middlewares/isValidId.js";
 import { authenticate } from "../middlewares/authenticate.js";
+import { checkRoles } from "../middlewares/checkRoles.js";
+import { ROLES } from "../constants/index.js";
 
 const studentsRouter = Router();
 
 studentsRouter.use(authenticate);
 
-studentsRouter.get("/", ctrlWrapper(getAllStudentsController));
+studentsRouter.get(
+  "/",
+  checkRoles(ROLES.TEACHER),
+  ctrlWrapper(getAllStudentsController)
+);
 studentsRouter.get(
   "/:studentId",
+  checkRoles(ROLES.TEACHER, ROLES.PARENT),
   isValidId,
   ctrlWrapper(getStudentByIdController)
 );
 studentsRouter.post(
   "/",
+  checkRoles(ROLES.TEACHER),
   validateBody(createStudentSchema),
   ctrlWrapper(createStudentController)
 );
 studentsRouter.delete(
   "/:studentId",
+  checkRoles(ROLES.TEACHER),
   isValidId,
   ctrlWrapper(deleteStudentController)
 );
 studentsRouter.put(
   "/:studentId",
+  checkRoles(ROLES.TEACHER, ROLES.PARENT),
   validateBody(updateStudentSchema),
   ctrlWrapper(upsertStudentController)
 );
