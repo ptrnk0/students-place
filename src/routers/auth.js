b3ee858f8a8a70806beae7f8ba99320c194dbc16
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-25 12:52:17 +0200

    feat: add reset password route with validation schema

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -4,6 +4,7 @@ import {
   loginUserSchema,
   registerUserSchema,
   requestResetEmailSchema,
+  resetPasswordSchema,
 } from "../validation/auth.js";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 import {
@@ -12,6 +13,7 @@ import {
   refreshUserSessionController,
   registerUserController,
   requestResetEmailController,
+  resetPasswordController,
 } from "../controllers/auth.js";
 
 const authRouter = Router();
@@ -29,11 +31,19 @@ authRouter.post(
 );
 
 authRouter.post("/logout", ctrlWrapper(logoutUserController));
+
 authRouter.post("/refresh", ctrlWrapper(refreshUserSessionController));
+
 authRouter.post(
   "/request-reset-email",
   validateBody(requestResetEmailSchema),
   ctrlWrapper(requestResetEmailController)
+);
+
+authRouter.post(
+  "/reset-password",
+  validateBody(resetPasswordSchema),
+  ctrlWrapper(resetPasswordController)
 );
 
 export default authRouter;
