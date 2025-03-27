2f39f28dbea62f223fdb2a0e3ab089b97fbb9e0d
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-27 19:49:19 +0200

    feat: create necessary directories on application startup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import { TEMP_UPLOAD_DIR, TEMPLATES_DIR } from "./constants/index.js";
 import { initMongoDB } from "./db/initMongoDB.js";
 import { startServer } from "./server.js";
+import { createDirIfNotExists } from "./utils/createDirIfNotExists.js";
 
 async function bootstrap() {
   await initMongoDB();
+  await createDirIfNotExists(TEMPLATES_DIR);
+  await createDirIfNotExists(TEMP_UPLOAD_DIR);
   startServer();
 }
 
