1d1875e4477635fff4587a72df133b3fe9323c0c
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-28 16:43:48 +0200

    feat: update bootstrap function to create upload directory instead of templates directory

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import { TEMP_UPLOAD_DIR, TEMPLATES_DIR } from "./constants/index.js";
+import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from "./constants/index.js";
 import { initMongoDB } from "./db/initMongoDB.js";
 import { startServer } from "./server.js";
 import { createDirIfNotExists } from "./utils/createDirIfNotExists.js";
 
 async function bootstrap() {
   await initMongoDB();
-  await createDirIfNotExists(TEMPLATES_DIR);
+  await createDirIfNotExists(UPLOAD_DIR);
   await createDirIfNotExists(TEMP_UPLOAD_DIR);
   startServer();
 }
