11b32945e59785f6d6c04569b81c2b7365306376
ptrnk0 <vlad.ptrnko@gmail.com>
2025-04-02 10:00:56 +0300

    feat: add validation for OAuth code and extract full name from token payload

--- src/utils/googleOAuth2.js
+++ src/utils/googleOAuth2.js
@@ -2,6 +2,7 @@ import { OAuth2Client } from "google-auth-library";
 import path from "node:path";
 import { readFile } from "node:fs/promises";
 import { getEnvVar } from "./getEnvVar.js";
+import createHttpError from "http-errors";
 
 const PATH_JSON = path.join(process.cwd(), "google-oauth.json");
 const oauthConfig = JSON.parse(await readFile(PATH_JSON));
@@ -19,4 +20,28 @@ export function generateAuthUrl() {
       "https://www.googleapis.com/auth/userinfo.profile",
     ],
   });
+}
+
+export async function validateCode(code) {
+  const response = await googleOAuthClient.getToken(code);
+  if (!response.tokens.id_token) {
+    throw createHttpError(401, "Unauthorized");
+  }
+
+  const ticket = await googleOAuthClient.verifyIdToken({
+    idToken: response.tokens.id_token,
+  });
+  return ticket;
+}
+
+export async function getFullNameFromGoogleTokenPayload(payload) {
+  let fullName = "Guest";
+
+  if (payload.given_name && payload.family_name) {
+    fullName = `${payload.given_name} ${payload.family_name}`;
+  } else if (payload.given_name) {
+    fullName = payload.given_name;
+  }
+
+  return fullName;
 }
